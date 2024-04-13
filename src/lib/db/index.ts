7a65d34e3d9eca5bf8378d44b5bc3174a8369590
troypoulter <troypoulter@gmail.com>
2024-04-13 17:32:09 +1000

    add console log to DB URL

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,8 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
+console.log("Database URL:", process.env.DATABASE_URL);
+
 const client = createClient({
   url: process.env.DATABASE_URL as string,
   authToken: process.env.DATABASE_AUTH_TOKEN as string,
